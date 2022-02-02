e6fe8814c0ebb5fcb1121dbc580eb6300fdf3556
GheorghiPatraman <gh.ptr.bblh@gmail.com>
2022-02-02 19:44:37 +0200

    TEST 2 IS CHANGED, NUMBER 3 IS ADDED

--- main.py
+++ main.py
@@ -11,8 +11,24 @@ print(string)
 # Number 2
 ip = str(input("Enter the IP Address: "))
 
-if re.match('[0-9]+(.[0-9]+){3}', ip):
+if re.search("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$", ip):
     ip = ip.replace(".", "[.]")
     print(ip)
 else:
     print("Invalid IP Address!")
+
+# Number 3
+arr = []
+
+n = int(input("Enter length of array: "))
+
+for i in range(0, n):
+    temp = int(input('Enter number: '))
+    arr.append(temp)
+print("Array: ", arr)
+
+arr.sort()
+print("Array: ", arr)
+
+arr.sort(reverse=True)
+print("Array: ", arr)
